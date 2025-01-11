cd3d25bb6387db05ed768d1c2a4f1e854d902539
Ved2604 <59091710+Ved2604@users.noreply.github.com>
2025-01-11 17:08:58 +0530

    remove Footer component and its usage from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Navbar from "./components/Navbar";
 import ExploreGrid from "./components/ExploreGrid";
-import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
       <div className="bg-gray-100">
         <Navbar />
         <ExploreGrid />
-        <Footer />
       </div>
     </>
   );
